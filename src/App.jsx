b928152cef8d359722763dfcd968046faacd4022
Sriramnath M <msriramnath3@gmail.com>
2025-12-16 18:41:43 +0530

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        -
+      </button>
+    </div>
+  );
+}
+
+export default App;
